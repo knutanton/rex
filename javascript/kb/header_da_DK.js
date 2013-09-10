f1354418df3464e42468970d490790d2b6f1a6fb
Hasse Feldthaus <hafe@kb.dk>
2013-09-10 16:12:06 +0200

    Added FIXME note.

--- javascript/kb/header_da_DK.js
+++ javascript/kb/header_da_DK.js
@@ -335,7 +335,7 @@ function trafiklys() {
     }
 }
 
-function startsWith(s,a) {
+function startsWith(s,a) { // FIXME: These helper methods shouldn't be dumped in window!
         for (x in a) {
                 if (s.indexOf(a[x]) === 0) {
                         return true;
