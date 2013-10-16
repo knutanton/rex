8d9b22a6eb3b363b1fa5958a2854e1bb087fe187
Jacob Larsen <jac@kb.dk>
2013-10-16 14:01:50 +0200

    included the css switcher function (bootstrap --> KGL)

--- javascript/kb/header_global.js
+++ javascript/kb/header_global.js
@@ -564,6 +564,21 @@ function removeUnWantedContent() {
     $('.EXLFooterUpdateContainer').remove();
 }
 
+
+/**
+* When on "Min konto" we dont have
+* a bootstrap structure, so we switch back to
+* the good alo css
+*/
+(function removeBootsrapCss() {
+   if (!(document.URL.indexOf('search.do') > -1 || document.URL.indexOf('display.do') > -1)) {
+    var currCssLink = $("link[href*='bootstrap.css']").attr("href");
+    var newCssLink = currCssLink.replace("bootstrap.css", "KGL.css");
+    $("link[href*='bootstrap.css']").attr("href", newCssLink);
+
+   }
+}());
+
 /*
 // Hey Hasse lidt til dig ;)
 $(document).ready(function () {
