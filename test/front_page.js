c1f99ebef75ea5510cae4fd4a3387817d5d32d2e
jacoblarsen.net <jac@kb.dk>
2014-02-04 10:48:38 +0100

    Update front_page.js
    made test pass

--- test/front_page.js
+++ test/front_page.js
@@ -3,7 +3,7 @@ module.exports = {
 	  test
 	    .open('http://primo-97.kb.dk/primo_library/libweb/action/search.do?dscnt=1&fromLogin=true&dstmp=1381382974048&vid=kbdev01&fromLogin=true')
 	    	.assert.text('#exlidUserAreaRibbon li', 'Ny låner', 'New user menu item in place')
-	    	.assert.numberOfElements('#exlidUserAreaRibbon li a', 8 , 'There exisits 10 menu items')
+	    	.assert.numberOfElements('#exlidUserAreaRibbon li a', 10 , 'There exisits 10 menu items')
 	    	.assert.doesntExist('#exlidPleaseWaitContainer', 'Please Wait container kindly removed from the UI')
 	    .done();
 	}
